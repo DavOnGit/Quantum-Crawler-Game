25261a5f6423e5d1d2bb11e31dfd946ecf2637bf
Davide Vozzi <davide.vozzi@gmail.com>
2017-02-14 02:20:20 +0100

    Add TodoApp.jsx

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+var TodoApp = React.createClass({
+  
+  render(){
+    return(
+      <div>
+        TodoApp.jsx
+      </div>
+    )
+  }
+})
+
+module.exports = TodoApp;
