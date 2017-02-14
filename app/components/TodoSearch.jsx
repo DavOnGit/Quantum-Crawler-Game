c0dfe49c16499ebd97cfcee52a187ba833fe0c71
Davide Vozzi <davide.vozzi@gmail.com>
2017-02-14 22:59:55 +0100

    Add Tests for search component

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -6,7 +6,7 @@ var TodoSearch = React.createClass({
     var showCompleted = this.refs.showCompleted.checked
     var searchText = this.refs.searchText.value
     
-    this.props.onSearch={showCompleted, searchText}
+    this.props.onSearch(showCompleted, searchText)
   },
   
   render: function(){
